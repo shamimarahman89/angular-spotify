29de2fde452ed85818fdc2117660737427718b8c
Shamima Rahman <Samu@Shamimas-MacBook-Air.local>
2021-05-03 22:28:11 -0400

    adding environment variables back

---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+
+  clientID:"1c12575e7f8e45cf8c5f2242ac794684",
+  clientSecret:"3806faf7e5394ea482f2d905a909e92e",
+  production: false,
+  userAPIBase: "https://spotify-user-api.herokuapp.com/api/user"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
