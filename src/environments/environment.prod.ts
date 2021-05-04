1ebc63574f081d75b920b9c0681880ccd88587bd
Shamima Rahman <Samu@Shamimas-MacBook-Air.local>
2021-05-03 22:19:02 -0400

    Remove unneccessary files

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-
-  clientID:"1c12575e7f8e45cf8c5f2242ac794684",
-  clientSecret:"3806faf7e5394ea482f2d905a909e92e",
-  production: true,
-  userAPIBase: "https://spotify-user-api.herokuapp.com/api/user"
-};
